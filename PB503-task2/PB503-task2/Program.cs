bcf0e20d60fd6efae872c26b2d5b25094ee2192c
Zumrud005 <mustafazuzu05@gmail.com>
2024-10-11 20:03:01 +0400

    created folder and task

---
+++ PB503-task2/PB503-task2/Program.cs
@@ -0,0 +1,124 @@
+﻿namespace PB503_task2
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            #region Bolme
+
+
+            //for (int i = 0; i <= 100; i++)
+            //{
+            //    if (i % 15 == 0)
+            //    {
+            //        Console.WriteLine("i= "+i);
+
+
+
+            //    }
+            //}
+            #endregion
+            #region Diplom
+            //int bal = 68;
+
+            //if (bal < 65)
+            //{
+            //     Console.WriteLine("kesildin");
+            //}
+            //else if (bal >= 65 && bal < 85)
+            //{
+            //    Console.WriteLine("adi diplom");
+
+            //}
+            //else if (bal >= 85 && bal < 95)
+            //{
+            //    Console.WriteLine("seref diplomu");
+
+            //}
+            //else if (bal >= 95 && bal <= 100)
+            //{
+            //    Console.WriteLine("yüksek şeref diplomu");
+            //}
+            #endregion
+            #region Arithmetic Operators
+            //int a = 90;
+            //int b = 34;
+            //int c = a + b;
+            //int d = a - b;
+            //Console.WriteLine(c);
+            //Console.WriteLine(d);
+            //Console.WriteLine("qismet=  " + a / b);
+            //Console.WriteLine("Hasil= " + a * b);
+            //Console.WriteLine("modul= " + a % b);
+
+            #endregion
+            #region Diplom2
+            //Console.WriteLine("Balinizi daxil edin: ");
+            //int bal = Convert.ToInt32(Console.ReadLine());
+
+            //if (bal < 65)
+            //{
+
+            //    Console.WriteLine("kesildin");
+            //}
+            //else if (bal >= 65 && bal < 85)
+            //{
+            //    Console.WriteLine("Adi diplom");
+
+            //}
+            //else if (bal >= 85 && bal < 95)
+            //{
+            //    Console.WriteLine("Seref diplomu");
+
+            //}
+            //else if (bal >= 95 && bal <= 100)
+            //{
+            //    Console.WriteLine("Yüksek Şeref Diplomu");
+            //}
+            #endregion
+            #region Assignment Operators
+            //int x = 47;
+            //x += 90;
+            //Console.WriteLine("toplama: "+ x);
+            //x *= 2;
+            //Console.WriteLine("vurma: "+ x);
+            //x/= 9; 
+            //Console.WriteLine("bolme:"+ x); 
+            //x %= 9;
+            //Console.WriteLine("modul: "+ x);
+
+
+
+            #endregion
+            #region Comparison Operators
+            //int a = 78;
+            //int b = 65;
+            //int c = 99;
+            //Console.WriteLine(a==b);
+            //Console.WriteLine(b==c);
+            //Console.WriteLine(c==a);
+            //Console.WriteLine(a!=b);
+            //if (a != b && c >= b)
+            //{
+            //    Console.WriteLine("Dogru beraberlikdir");
+            //}
+            //if (a<b && b < c)
+            //{
+            //    Console.WriteLine("b a ve c arasindadir");
+
+            //}
+
+            #endregion
+            #region Logical Operators
+            //int x = 23;
+            //int y = 2023;
+            //bool check= (x == y || x<y);
+            //Console.WriteLine(check);
+            //Console.WriteLine(!check);
+            //check= (x <= y && y>x);
+            //Console.WriteLine(check);   
+            #endregion
+        }
+    }
+}
+    
